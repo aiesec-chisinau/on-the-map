3f5cb52b540640f892a92de23eb163adedd37c3d
Cristian Brinza <cristian.brinza.service.acc@gmail.com>
2023-01-15 12:50:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,3 @@
 
 document.getElementById("hidel").style.display='none';
 
-function dischange(){
-   
-    document.getElementById("pageiddiv").style.display='block';
-    document.getElementById("preloadpage").style.display='none';
-}
-
-function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
-  }
-  
-document.getElementById("pageiddiv").style.display='none';
-document.getElementById("preloadpage").style.display='block'
-delay(2500).then(() => 
-    dischange()
-);
