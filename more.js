88c892101ab1dd81ffec916bd69e8eb0c3626b60
Cristian Brinza <cristian.brinza.service.acc@gmail.com>
2023-02-02 21:51:38 +0200

    Create more.js

---
+++ more.js
@@ -0,0 +1,69 @@
+var a1=0;
+    function a(){
+        if (a1==0){
+            document.getElementById('1a').style.transform = 'rotate(180deg)';
+            document.getElementById("1b").style.display='block';
+            a1=1;
+        }
+        else{
+            document.getElementById('1a').style.transform = 'rotate(0deg)';
+            document.getElementById("1b").style.display='none';
+            a1=0;
+        }
+    }
+
+    var b2=0;
+    function b(){
+        if (b2==0){
+            document.getElementById('2a').style.transform = 'rotate(180deg)';
+            document.getElementById("2b").style.display='block';
+            b2=1;
+        }
+        else{
+            document.getElementById('2a').style.transform = 'rotate(0deg)';
+            document.getElementById("2b").style.display='none';
+            b2=0;
+        }
+    }
+
+    var c3=0;
+    function c(){
+        if (c3==0){
+            document.getElementById('3a').style.transform = 'rotate(180deg)';
+            document.getElementById("3b").style.display='block';
+            c3=1;
+        }
+        else{
+            document.getElementById('3a').style.transform = 'rotate(0deg)';
+            document.getElementById("3b").style.display='none';
+            c3=0;
+        }
+    }
+
+    var d4=0;
+    function d(){
+        if (d4==0){
+            document.getElementById('4a').style.transform = 'rotate(180deg)';
+            document.getElementById("4b").style.display='block';
+            d4=1;
+        }
+        else{
+            document.getElementById('4a').style.transform = 'rotate(0deg)';
+            document.getElementById("4b").style.display='none';
+            d4=0;
+        }
+    }
+
+    var e5=0;
+    function e(){
+        if (e5==0){
+            document.getElementById('5a').style.transform = 'rotate(180deg)';
+            document.getElementById("5b").style.display='block';
+            e5=1;
+        }
+        else{
+            document.getElementById('5a').style.transform = 'rotate(0deg)';
+            document.getElementById("5b").style.display='none';
+            e5=0;
+        }
+    }
