4446eb5e8ab1a1c79af7d623145ba97c8a8aabfd
Cristian Brinza <cristian.brinza.service.acc@gmail.com>
2023-02-26 21:20:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,8 @@
 
 
 document.getElementById("hidel").style.display='none';
-
+if(JSON.parse(localStorage.loginvalid)==true){
+document.getElementById("logindashiddiv").innerHTML='Dash →';
+document.getElementById("logindashiddiv").setAttribute('href',"dashboard.html");
+}
+else document.getElementById("logindashiddiv").innerHTML='Sign In →';
